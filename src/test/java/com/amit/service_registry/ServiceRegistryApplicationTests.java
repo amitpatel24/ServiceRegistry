31d05b7a0da0436949a2cc086fcebe7692c1853c
amitpatel24 <patel.amit@hotmail.com>
2024-10-11 23:38:52 +0530

    Service Registry Project

---
+++ src/test/java/com/amit/service_registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amit.service_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
